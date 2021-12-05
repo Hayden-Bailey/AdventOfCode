882718b7a365a55c89bbceca696fc9fa4ff4d9db
Hayden-Bailey <hayden.bailey@my.jcu.edu.au>
2021-12-05 20:38:31 +1000

    Update D4P1.java
    Add comments for process and preliminary bingo code

--- Day4/D4P1.java
+++ Day4/D4P1.java
@@ -7,18 +7,45 @@ import java.util.List;
 
 public class D4P1 {
     public static void main(String[] args) throws IOException  {
+        // Extract Bingo Numbers into Int Array
         File file = new File("AdventOfCode\\Day4\\input.txt");
-
         BufferedReader br = new BufferedReader(new FileReader(file));
         String inputString = br.readLine();
         String[] questionOrder = inputString.split(",");
+        int[] bingoNumbers = new int[questionOrder.length];
+        for (int i = 0; i < questionOrder.length; i++) {
+            bingoNumbers[i] = Integer.parseInt(questionOrder[i]);
+        }
         br.close();
 
+        // Extract Bingo Boards into Arraylist of 2d integer arrays
         ArrayList<int[][]> bingoBoards = new ArrayList<int[][]>();
 
         BufferedReader reader = new BufferedReader(new FileReader(file));
         reader.readLine();
+        saveBingoBoards(bingoBoards, reader);
+        
 
+        // Iterate through Bingo Numbers, replace Bingo number on boards with '0'
+        for (Integer number : bingoNumbers) {
+            // Replace bingo number on all boards
+            for (int[][] bingoBoard : bingoBoards) {
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        if (bingoBoard[i][j] == number) {
+                            bingoBoard[i][j] = 0;
+                        }
+                    }
+                }
+            }
+
+            // Within for loop, add check to all boards for bingo
+            
+        }
+    }
+
+
+    private static void saveBingoBoards(ArrayList<int[][]> bingoBoards, BufferedReader reader) throws IOException {
         while (reader.readLine() != null) {
             int[][] arr = new int[5][5];
             for (int i = 0; i < 5; i++) {
@@ -42,7 +69,5 @@ public class D4P1 {
             bingoBoards.add(arr);
         }
         reader.close();
-
-        
     }
 }
